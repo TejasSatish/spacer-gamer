484b0d41a7b28aedb1ce3b7c1ee6ba62f684b6ed
AdityaSrinivasN <aditya.srinivas2016@gmail.com>
2024-02-25 20:35:57 +0530

    Added Movement and Physics

--- js/test.js
+++ js/test.js
@@ -1,41 +1,68 @@
+import { playerSpeed } from "./constants"; 
 export class Example extends Phaser.Scene
 {
+    
     constructor ()
     {
         super();
+        this.player
+        this.cursor
+        this.playerSpeed
+          
     }
 
     preload ()
     {
         
-        this.load.spritesheet('sussy', 'assets/pngegg.png', { frameWidth: 1920, frameHeight: 1080});
-        this.load.spritesheet('coin', 'assets/pngwing.com.png', { frameWidth: 1920, frameHeight: 1080 });
-    
+        this.load.image("bg", "assets/spacebg.jpg")
+        this.load.image("ship", "assets/Shiptest.png");
+
     }
 
     create ()
     {
+        const bg = this.add.image(0,0,"bg").setOrigin(0,0)
+        bg.setDisplaySize(this.game.config.width, this.game.config.height);
         
-        // const x = Phaser.Math.Between(100, 700);
-        // const y = Phaser.Math.Between(100, 500);
-        // const x1 = Phaser.Math.Between(100, 700);
-        // const y2 = Phaser.Math.Between(100, 500);
-        const x = 300
-        const y = 500
-        const x1 = 230
-        const y1 = 130
-        const frame = Phaser.Math.Between(0, 4);
-        const frame2 = Phaser.Math.Between(0, 4);
-        this.add.sprite(x, y, 'sussy', frame)
-        this.add.sprite(x1, y1, 'coin', frame2)
-    }
+        this.player = this.physics.add.image(window.innerWidth/2,window.innerHeight*0.8,"ship").setOrigin(0,0)
+        this.player.setScale(0.05,0.05)
+        this.player.setImmovable(true)
+        this.player.body.allowGravity = false
+        this.player.setCollideWorldBounds(true)
 
 
-    addAnimation (key)
-    {
-        this.add.sprite(400, this.y, 'sussy')
-            .play(key);
-        this.y += 100;
+        this.cursor = this.input.keyboard.createCursorKeys()
     }
+
+    update(){
+        
+        const { left, right, up, down } = this.cursor;
+     
+
+        if (left.isDown && right.isDown) {
+            this.player.setVelocityX(0);
+            
+        } else if (left.isDown) {
+            this.player.setVelocityX(-playerSpeed);
+            
+        } else if (right.isDown) {
+            this.player.setVelocityX(playerSpeed);
+            
+        } else if (up.isDown && down.isDown) {
+            this.player.setVelocityY(0);
+            
+        } else if (up.isDown) {
+            this.player.setVelocityY(-playerSpeed);
+            
+        } else if (down.isDown) {
+            this.player.setVelocityY(playerSpeed);
+            
+        } else {
+            this.player.setVelocityX(0);
+            this.player.setVelocityY(0);
+        }
+        
+    }
+
 }
 
--- main.js
+++ main.js
@@ -6,12 +6,21 @@ const browser={
   height: window.innerHeight
 }
 
+//const speedDown = 300
+
 const config = {
   type:Phaser.WEBGL,
   width:browser.width,
   height:browser.height,
   canvas:canvas,
-  scene: Example
+  scene: Example,
+  physics:{
+    default:"arcade",
+    arcade:{
+      //gravity:{y:speedDown},
+      debug:true
+    }
+  }
 }
 
 const game= new Phaser.Game(config)
